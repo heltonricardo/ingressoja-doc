9ece138245172aa096cf6b6ec9aa381ae86ef5af
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-11-30 19:22:02 -0300

    arquivos de projeto de componentes

---
+++ Projeto de Componentes/Gerar Imagens.bat
@@ -0,0 +1,12 @@
+@echo off
+title PlanUML
+
+echo Ao finalizar a renderizacao das imagens, feche essa janela.
+
+:: Coloque esse arquivo no diretório dos diagramas e o execute.
+
+:: Local do programa
+set PLAN_UML_DIR="..\Programas\plantuml.jar"
+
+java -jar %PLAN_UML_DIR% -charset UTF-8 -gui "%cd%"
+exit
